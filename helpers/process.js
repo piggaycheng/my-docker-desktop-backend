acc82ea453a5b66120d425962e3e165a20bcdba4
piggaycheng <piggaycheng123@gmail.com>
2023-06-06 22:41:57 +0800

    getSubProcess()

--- helpers/process.js
+++ helpers/process.js
@@ -1,14 +1,19 @@
 const { spawn } = require("child_process")
 
-let subProcess = null
-if (process.platform === 'win32') {
-    const wslProcess = spawn("wsl", ["-u", "root"])
-    wslProcess.stdin.write("service docker start\n")
-    subProcess = wslProcess
-} else if (process.platform === 'darwin') {
-    const bashProcess = spawn("bash")
-    subProcess = bashProcess
+const getSubProcess = () => {
+    if (process.platform === 'win32') {
+        const wslProcess = spawn("wsl", ["-u", "root"])
+        wslProcess.stdin.write("service docker start\n")
+        return wslProcess
+    } else if (process.platform === 'darwin') {
+        const bashProcess = spawn("bash")
+        return bashProcess
+    }
+
+    return null
 }
+
+const subProcess = getSubProcess()
 
 module.exports = {
     subProcess
